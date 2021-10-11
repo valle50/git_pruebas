2f5cfda8231c8084b80deedee9cbcbabb79dc080
Chris <cmacias@zegucom.com.mx>
2021-10-11 09:53:58 -0500

    sube cambios

--- doc1.php
+++ doc1.php
@@ -1,37 +1,48 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pruebas</title>
 </head>
+
 <body>
     <h1>Documento de prueba</h1>
     <h6> Pruebas </h6>
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <input type="text" name="" class="clase">
             </div>
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <input type="text" name="" class="clase">
             </div>
-            <div class="col-md-4">
+            <div class="col-md-3">
+                <input type="text" name="" class="clase">
+            </div>
+            <div class="col-md-3">
                 <input type="text" name="" class="clase">
             </div>
         </div>
     </div>
-    
-    <script>
-        $(document).ready(function(){
 
-            $('.clase').keyup(function(){
-                console.log($(this).val() );
+    <script>
+        $(document).ready(function() {
+
+            $('.clase').keyup(function() {
+                console.log($(this).val());
                 alert("");
                 console.warn("prueba");
             });
         });
     </script>
+    <script>
+        $(document).ready(function() {
+            alert("hola");
+        });
+    </script>
 </body>
+
 </html>
