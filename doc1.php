71fbe6f9bbb917f015a0a81851d16364333f6e70
Chris <cmacias@zegucom.com.mx>
2021-10-12 14:34:52 -0500

    cambios nueva rama

--- doc1.php
+++ doc1.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pruebas</title>
+    <title>Pruebas2</title>
 </head>
 
 <body>
     <h1>Documento de prueba</h1>
-    <h6> Pruebas </h6>
-    <p>parrafo</p>
+    <h6> Pruebas2 </h6>
     <div class="container">
         <div class="row">
             <div class="col-md-3">
@@ -33,15 +31,14 @@
         $(document).ready(function() {
 
             $('.clase').keyup(function() {
-                console.log($(this).val());
-                alert("");
-                console.warn("prueba");
+                alert("hola");
             });
         });
     </script>
+
     <script>
         $(document).ready(function() {
-            alert("hola");
+            console.log("hola");
         });
     </script>
 </body>
